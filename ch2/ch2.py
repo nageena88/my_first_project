ce56b05851556b32b64c166a4050a20ab3448e33
nageena88 <ram.nageena88@hotmail.com.com>
2025-07-19 13:04:29 +0530

    fix: release new chapter ch2

---
+++ ch2/ch2.py
@@ -0,0 +1,57 @@
+# # data objects
+# x = 12
+# print(type(x))
+#
+# x = "Ram"
+# print(type(x))
+#
+# x = 'This is a "python" classes.'
+# print(x)
+# print(type(x))
+#
+# x = "I can't run"
+# print(x)
+# x = "This is a statements.\
+# This is another objects.\
+# This is third statements test objects."
+#
+# print(x)
+#
+# x = 12.56
+# print(type(x))
+# print(x)
+#
+# x = 12 + 5j
+# print(type(x))
+# print(x)
+#
+# x = False
+# print(type(x))
+# print(x)
+
+# list object
+# lst = [1, 2, 3, [78, 6], "Ram", 56.7, True]
+# print(type(lst))
+# print(lst)
+
+# t = (1, 2, 3, [78, 6], "Ram", 56.7, True)
+# t = (1,)
+# print(type(t))
+# print(t)
+
+# dict
+
+# d = {"Name": "Yash", "Age": 32}
+# print(type(d))
+# print(d)
+
+# set
+
+# a = set()
+# print(type(a))
+# print(a)
+
+
+a = {1, 2, 3, 1, 2, 3, 4}
+print(type(a))
+print(a)
---
+++ ch2/op.py
@@ -0,0 +1,34 @@
+# Arithmetics operators
+# +, -, *, /, //, **, %
+
+x, y = 60, 13
+print(x)
+print(y)
+
+# res = x + y
+#
+# print(res)
+#
+# print(x - y)
+#
+# print(x / y)
+# print(x * y)
+# print(x // y)
+# print(x % y)
+# print(2**5)
+
+# Relational operators
+# >, >=, <, <=, ==, !=
+
+# res = x > y
+# print(res)
+#
+# res = x >= 60
+# print(res)
+#
+# print(x < y)
+#
+# print(x <= y)
+#
+# print(x == y)
+# print(x != y)
